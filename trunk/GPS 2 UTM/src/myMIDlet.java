ca5d59e4f96a5d544686bc0ed8a9c8588f7e40ca
yagluli <yagluli@cbd5fe53-c971-715d-3abf-cac841e07a84>
2010-04-25 17:38:30 +0000

    I added Commands and updates

--- trunk/GPS 2 UTM/src/myMIDlet.java
+++ trunk/GPS 2 UTM/src/myMIDlet.java
@@ -1,23 +1,38 @@
 import gps2itm.Convertor;
 import gps2itm.LatLon;
 
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
 import javax.microedition.location.Coordinates;
 import javax.microedition.location.Criteria;
 import javax.microedition.location.Location;
+import javax.microedition.location.LocationException;
+import javax.microedition.location.LocationListener;
 import javax.microedition.location.LocationProvider;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
+public class myMIDlet extends MIDlet implements CommandListener , LocationListener {
 
-public class myMIDlet extends MIDlet {
-	
+	Command exit, about;
 	Display display;
 	Form form;
-	
-	public myMIDlet() {
-		// TODO Auto-generated constructor stub
+	LocationProvider lp;
+	Alert alTest;
+	public myMIDlet() throws LocationException {
+		form = new Form("GPS to ITM");
+		initiateCommands();
+		form.setCommandListener(this);
+		Criteria cr = new Criteria();
+		cr.setHorizontalAccuracy(20);		// accurate to 20 meters horizontally
+		lp = LocationProvider.getInstance(cr);		// Now get an instance of the provider
+		lp.setLocationListener(this, 180, -1, -1);
+		
 	}
 
 	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
@@ -31,49 +46,77 @@ public class myMIDlet extends MIDlet {
 	}
 
 	protected void startApp() throws MIDletStateChangeException {
-    
-	form = new Form("GPS to ITM");
 		
-	LatLon ortalLatlon;
-	try {
-		ortalLatlon = getCoordinates();
-		
-		form.append(Convertor.numbersToLatLon(ortalLatlon) );
-		
-		
-		display = Display.getDisplay(this);
-	    display.setCurrent(form);
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
+
 	}
 
-	public static LatLon getCoordinates() throws Exception
-	{
-	
-	Criteria cr= new Criteria();
-	
-	cr.setHorizontalAccuracy(20);  // accurate to 20 meters horizontally
+	private void updateDisplay(Location location) {
+		LatLon ortalLatlon;
+		try {
+			ortalLatlon = getCoordinates(location);
 
-	
-		// Now get an instance of the provider
-		LocationProvider lp= LocationProvider.getInstance(cr);
+			form.append(Convertor.numbersToLatLon(ortalLatlon));
+
+			display = Display.getDisplay(this);
+			display.setCurrent(form);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void initiateCommands() {
+		about = new Command("about", Command.ITEM, 0);
+		exit = new Command("Exit", Command.EXIT, 1);
+		form.addCommand(about);
+		form.addCommand(exit);
+
+	}
+
+	public  LatLon getCoordinates(Location l) throws Exception {
+
 		
-		// Request the location, setting a one-minute timeout
-		Location l = lp.getLocation(60);
 		Coordinates c = l.getQualifiedCoordinates();
-		
-		if(c != null ) {
+
+		if (c != null) {
 			// Get coordinate information
 			double lat = c.getLatitude();
 			double lon = c.getLongitude();
 
 			// Now recenter map to given location, zoom to street level
 			return (new LatLon(lat, lon));
-		}	
-		    throw new Exception();
-	
+		}
+		throw new Exception();
+
+	}
+
+	public void commandAction(Command c, Displayable d) {
+
+		if (c == exit) {
+		      try {
+				destroyApp(true);
+			} catch (MIDletStateChangeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		      notifyDestroyed();		}
+		if (c == about) {
+			
+			alTest = new Alert("About", "This inguines program was made by Yuval & Yagel Kohavi", null,
+                     AlertType.INFO);
+			display.setCurrent(alTest,form);
+		}
+	}
+
+	public void locationUpdated(LocationProvider locationProvider, Location location) {
+
+		
+		updateDisplay(location);
+		
+	}
+
+	public void providerStateChanged(LocationProvider locationProvider, int newState ) {
+		// TODO Auto-generated method stub
+		
 	}
 }
