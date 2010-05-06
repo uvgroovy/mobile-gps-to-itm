da4f3181b3bedcc1116a37a323dc750494cd1075
yuval.kohavi <yuval.kohavi@cbd5fe53-c971-715d-3abf-cac841e07a84>
2010-05-06 09:19:30 +0000

    Comment fixes

--- trunk/GPS 2 UTM/src/gps2itm/LatLon.java
+++ trunk/GPS 2 UTM/src/gps2itm/LatLon.java
@@ -1,55 +1,73 @@
 package gps2itm;
 
-
 public class LatLon {
 	double lat;
 	double lng;
-	 int alt;
-	 int  precision;
+	int alt;
 
-	 public LatLon(double lat, double lng,int precision){
-	     this.lat = lat;
-	     this.lng = lng;
-	     this.alt = 0;
-	     this.precision = precision;// || 5;
-	 }
-
+	/**
+	 * Creates a new LatLng.
+	 * 
+	 * @class holds geographic coordinates measured in degrees.<br/>
+	 *        <a
+	 *        href="http://en.wikipedia.org/wiki/Geographic_coordinate">http:/
+	 *        /en.wikipedia.org/wiki/Geographic_coordinate</a>
+	 * 
+	 * @constructor
+	 * @param lat
+	 *            latitude in degrees ( http://en.wikipedia.org/wiki/Latitude )
+	 * @param lng
+	 *            longtitude in degrees ( http://en.wikipedia.org/wiki/Longitude
+	 *            )
+	 */
 	public LatLon(double lat, double lng) {
-		super();
 		this.lat = lat;
 		this.lng = lng;
+		this.alt = 0; // altitude in meters above the uesd geoid surface - this
+		// was not used or tested - please keep this always 0.
 	}
-	
-	 public String toString ()
-	 { 
-	     return this.lat + " " + this.lng ;
-	 }
 
+	/**
+	 * Returns Latlng as string.
+	 * 
+	 * @return {String}
+	 */
+	public String toString() {
+		return this.lat + " " + this.lng;
+	}
 
-	 /**
-	  * Creates a new LatLng by converting coordinates from a source ellipsoid to a target one. <p>
-	  * This process involves:<p>
-	  *   1. converting the angular latlng to cartesian coordinates using latLngToPoint()<p>
-	  *   2. translating the XYZ coordinates using a translation, with special values supplied to this translation.<p>
-	  *   3. converting the translated XYZ coords back to a new angular LatLng<p>
-	  *<p>
-	  *  for example refer to {@link JSITM.itm2gps}
-	  *
-	  * @param {JSITM.Ellipsoid} from source elli
-	  * @param {JSITM.Ellipsoid} to
-	  * @param {JSITM.Translation} translation
-	  * @return {JSITM.LatLng}
-	  */
-	 
-	 public LatLon convertGrid(Ellipsoid from , Ellipsoid to, Translation translation ) 
-	 {
-	 
-	    Point point = from.latLngToPoint(this);
-	     
-	     //removed 7 point Helmert translation (not needed in Israel's grids)
-	     Point  translated = translation.translate(point);
-	     
-	     return to.pointToLatLng(translated);
-	 }
-	 
+	/**
+	 * Creates a new LatLng by converting coordinates from a source ellipsoid to
+	 * a target one.
+	 * <p>
+	 * This process involves:
+	 * <p>
+	 * 1. converting the angular latlng to cartesian coordinates using
+	 * latLngToPoint()
+	 * <p>
+	 * 2. translating the XYZ coordinates using a translation, with special
+	 * values supplied to this translation.
+	 * <p>
+	 * 3. converting the translated XYZ coords back to a new angular LatLng
+	 * <p>
+	 *<p>
+	 * for example refer to {@link JSITM.itm2gps}
+	 * 
+	 * @param {JSITM.Ellipsoid} from source elli
+	 * @param {JSITM.Ellipsoid} to
+	 * @param {JSITM.Translation} translation
+	 * @return {JSITM.LatLng}
+	 */
+
+	public LatLon convertGrid(Ellipsoid from, Ellipsoid to,
+			Translation translation) {
+
+		Point point = from.latLngToPoint(this);
+
+		// removed 7 point Helmert translation (not needed in Israel's grids)
+		Point translated = translation.translate(point);
+
+		return to.pointToLatLng(translated);
+	}
+
 }
--- trunk/GPS 2 UTM/src/gps2itm/Point.java
+++ trunk/GPS 2 UTM/src/gps2itm/Point.java
@@ -1,21 +1,47 @@
 package gps2itm;
 
 public class Point {
-double x;
-double y;
-double z;
+	double x;
+	double y;
+	double z;
 
-public Point(double x2, double y2, double z2) {
-	super();
-	this.x = x2;
-	this.y = y2;
-	this.z = z2;
+	/**
+	 * Creates a new Point.
+	 * 
+	 * @class holds 2D/3D cartesian coordinates.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public Point(double x2, double y2, double z2) {
+		super();
+		this.x = x2;
+		this.y = y2;
+		this.z = z2;
+	}
+
+
+	 /**
+	  * Returns a string containing the Point coordinates.
+	  */
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + ", z=" + z + "]";
+	}
+
+
+	public double getX() {
+		return x;
+	}
+
+
+	public double getY() {
+		return y;
+	}
+
+
+	public double getZ() {
+		return z;
+	}
+
 }
-
-public String toString() {
-	return "Point [x=" + x + ", y=" + y + ", z=" + z + "]";
-}
-
-
-}
-
