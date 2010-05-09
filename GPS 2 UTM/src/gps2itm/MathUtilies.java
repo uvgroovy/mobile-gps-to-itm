497f0a688aa8f9908c7d2f9d47408dc83bee40a9
yuval.kohavi <yuval.kohavi@cbd5fe53-c971-715d-3abf-cac841e07a84>
2010-05-09 17:15:14 +0000

    Added support for negative exponent Various gui fixes

--- GPS 2 UTM/src/gps2itm/MathUtilies.java
+++ GPS 2 UTM/src/gps2itm/MathUtilies.java
@@ -1,30 +1,34 @@
 package gps2itm;
 
 public class MathUtilies {
-	
+
 	/**
 	 * Raises base with the exponent
+	 * 
 	 * @param base
 	 * @param exponent
-	 * @return
+	 * @return base raised in the exponent (base**exponent)
 	 */
-	static public double pow ( double base, int exponent)
-	{
-		double sum=1;
-	
-		for (int i = 0; i < exponent; i++ )
-			sum *= base;
-		
-		return sum;
+	static public double pow(double base, int exponent) {
+		double product = 1;
+
+		int positiveExponent = Math.abs(exponent);
+		for (int i = 0; i < positiveExponent; i++)
+			product *= base;
+
+		if (exponent < 0)
+			product = 1.0 / product;
+		return product;
 	}
 
 	/**
 	 * No aTam2 in j2me. if you use j2se change this to ues the default one.
+	 * 
 	 * @param y
 	 * @param x
 	 * @return
 	 */
-	 static public double aTan2(double y, double x) {
+	static public double aTan2(double y, double x) {
 		return mMath.atan2(y, x);
 	}
 }
--- GPS 2 UTM/src/myMIDlet.java
+++ GPS 2 UTM/src/myMIDlet.java
@@ -1,6 +1,9 @@
 import gps2itm.Convertor;
 import gps2itm.LatLon;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Command;
@@ -21,115 +24,38 @@ import javax.microedition.midlet.MIDletStateChangeException;
 public class myMIDlet extends MIDlet implements CommandListener,
 		LocationListener {
 
-	Command exit, about;
-	Display display;
-  	Form form;
-	LocationProvider lp;
 	Alert alTest;
-	
-	StringItem coordinates;
+	Display display;
+	Command exit, about;
+	Form form;
+	StringItem gpsCoordinates;
+
 	StringItem height;
+	StringItem itmCoordinates;
+	StringItem lastUpdate;
+	LocationProvider lp;
 
 	public myMIDlet() throws LocationException {
 		Criteria cr = new Criteria();
 		cr.setHorizontalAccuracy(20); // accurate to 20 meters horizontally
-		// cr.setVerticalAccuracy(20); DO NOT add this line, it causes the software not to work!
+		// cr.setVerticalAccuracy(20); DO NOT add this line, it causes the
+		// software not to work!
 
 		lp = LocationProvider.getInstance(cr); // Now get an instance of the
 
+		// should be called after we tried to init the lp.
 		initForm();
 	}
 
-	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
-		// TODO Auto-generated method stub
-
-	}
-
-	protected void pauseApp() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void initApp() {
-		display = Display.getDisplay(this);
-
-		display.setCurrent(form);
-
-		form.setCommandListener(this);
-
-		if (lp == null) {
-			form.append("No location provider. This is a problem.");
-		} else {
-
-			lp.setLocationListener(this, 180, -1, -1); // Notify us every 3
-														// minutes
-		}
-	}
-
-	private void initForm() {
-		form = new Form("GPS to ITM");
-		form.append("Welcome! Data updates every 3 minutes or so..\n");
-	
-		coordinates = new StringItem("ITM:", "Please wait...");
-		height = new StringItem("Height:", "Please wait...");
-		
-		form.append(coordinates);
-		form.append(height);
-		initiateCommands();
-	}
-
-	protected void startApp() throws MIDletStateChangeException {
-
-		if (display == null) {
-			initApp();
-
-		}
-
-	}
-
-	private void updateDisplay(Location location) {
-		LatLon ortalLatlon;
-		try {
-			ortalLatlon = getCoordinates(location);
-			coordinates.setText(Convertor.convertGpsToITM(ortalLatlon));
-			height.setText(location.getQualifiedCoordinates().getAltitude() + "m");
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void initiateCommands() {
-		about = new Command("about", Command.ITEM, 0);
-		exit = new Command("Exit", Command.EXIT, 1);
-		form.addCommand(about);
-		form.addCommand(exit);
-
-	}
-
-	public LatLon getCoordinates(Location l) throws Exception {
-
-		Coordinates c = l.getQualifiedCoordinates();
-
-		if (c != null) {
-			// Get coordinate information
-			double lat = c.getLatitude();
-			double lon = c.getLongitude();
-			
-			return (new LatLon(lat, lon));
-		}
-		throw new Exception();
-
-	}
-
+	/**
+	 * React to user commands.
+	 */
 	public void commandAction(Command c, Displayable d) {
 
 		if (c == exit) {
 			try {
 				destroyApp(true);
 			} catch (MIDletStateChangeException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			notifyDestroyed();
@@ -143,6 +69,139 @@ public class myMIDlet extends MIDlet implements CommandListener,
 		}
 	}
 
+	protected void startApp() throws MIDletStateChangeException {
+		if (display == null) {
+			initApp();
+		}
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+
+	}
+
+	protected void pauseApp() {
+
+	}
+
+	/**
+	 * Init the application. call this method from startApp
+	 */
+	private void initApp() {
+		display = Display.getDisplay(this);
+		display.setCurrent(form);
+
+		if (lp != null) {
+			lp.setLocationListener(this, 180, -1, -1); // Notify us every 3
+			// minutes
+		}
+	}
+
+	/**
+	 * Init the gui.
+	 */
+	private void initForm() {
+		form = new Form("GPS to ITM");
+		form.append("Welcome! Data updates every 3 minutes or so..\n");
+
+		initCommands();
+
+		if (lp == null) {
+			form.append("No location provider fits. This is a problem.");
+		} else {
+			itmCoordinates = new StringItem("ITM:", "Please wait...");
+			height = new StringItem("Height:", "Please wait...");
+			lastUpdate = new StringItem("Last update:", "Please wait...");
+			form.append(itmCoordinates);
+			form.append(height);
+			form.append(lastUpdate);
+		}
+	}
+
+	/**
+	 * Updates the GUI. called when we have new results from the GPS.
+	 * 
+	 * @param location
+	 *            The location from the GPS
+	 */
+	private void updateDisplay(Location location) {
+
+		lastUpdate.setText(getTimeAsString());
+		try {
+			itmCoordinates.setText(Convertor
+					.convertGpsToITM(getCoordinates(location)));
+			height.setText(location.getQualifiedCoordinates().getAltitude()
+					+ "m");
+
+		} catch (Exception e) {
+			itmCoordinates.setText("An error has occoured.");
+			height.setText("");
+		}
+	}
+
+	/**
+	 * @return Current hour and minute: "HH:MM"
+	 */
+	private String getTimeAsString() {
+		Date d = new Date();
+
+		Calendar c =  Calendar.getInstance();
+
+		c.setTime(d);
+
+		String minute = "" + c.get(Calendar.MINUTE);
+		
+		if (minute.length() == 1) {
+			minute = "0" + minute;
+		}
+		
+		String time = c.get(Calendar.HOUR_OF_DAY) + ":"
+				+ minute;
+		return time;
+	}
+
+	/**
+	 * Initialize the commands for the user.
+	 */
+	private void initCommands() {
+		about = new Command("about", Command.ITEM, 0);
+		exit = new Command("Exit", Command.EXIT, 1);
+		form.addCommand(about);
+		form.addCommand(exit);
+
+		form.setCommandListener(this);
+
+	}
+
+	/**
+	 * Return the coordinates in LatLon object
+	 * 
+	 * @param l
+	 *            The current location
+	 * @return The lat and lon from the current location in LatLon object
+	 * @throws Exception
+	 *             If on coordinates were found in l.
+	 */
+	private LatLon getCoordinates(Location l) throws Exception {
+
+		Coordinates c = l.getQualifiedCoordinates();
+
+		if (c != null) {
+			// Get coordinate information
+			double lat = c.getLatitude();
+			double lon = c.getLongitude();
+
+			return (new LatLon(lat, lon));
+		}
+		throw new Exception();
+
+	}
+
+	/**
+	 * This is a callback for the Location API. This is called when the location
+	 * API has new location data.
+	 * 
+	 * @location Our current location
+	 */
 	public void locationUpdated(LocationProvider locationProvider,
 			Location location) {
 		// Only update if you have something valid to update.
@@ -156,4 +215,5 @@ public class myMIDlet extends MIDlet implements CommandListener,
 		// Intentionally left blank.
 
 	}
+
 }
